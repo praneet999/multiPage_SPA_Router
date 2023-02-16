f2274a14c3999b5ffdb8a17371d92510638bd2f1
Praneet999 <praneet.kshirasagar@gmail.com>
2023-02-16 23:04:56 +0530

    recent changes added

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Home";
+const router = createBrowserRouter([{ path: "/", element: <Homepage /> }]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+function Homepage() {
+  return <h1>My home page</h1>;
+}
+
+export default Homepage;
